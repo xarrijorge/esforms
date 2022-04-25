d819ba4cfa3c4906967f69e0df5e0fb5239c6c3b
xarrijorge <kodinglife@gmail.com>
2022-04-25 10:58:46 +0000

    Disabled past dates

--- src/components/BasicDatePicker.js
+++ src/components/BasicDatePicker.js
@@ -12,6 +12,7 @@ export default function BasicDatePicker({
     return (
         <LocalizationProvider dateAdapter={AdapterDateFns}>
             <DatePicker
+                disablePast
                 label={label}
                 value={dateValue}
                 name='date'
