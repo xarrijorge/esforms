d0e0fa31cd908060cafed5bd06934bd9184661e1
xarrijorge <kodinglife@gmail.com>
2022-06-29 22:46:45 +0000

    fixed lint errors

--- src/App.js
+++ src/App.js
@@ -1,14 +1,15 @@
-import React from 'react'
-import axios from 'axios'
-import { useNavigate } from 'react-router-dom'
-import Home from './components/Home'
+import React from 'react';
+import axios from 'axios';
+import { useNavigate } from 'react-router-dom';
+import Home from './components/Home';
 
-import './App.css'
+import './App.css';
 
 function App() {
-    const [loading, setLoading] = React.useState(false)
-    const CLIENT_ID = process.env.REACT_APP_CLIENT_ID
-    let navigate = useNavigate()
+    const [loading, setLoading] = React.useState(false);
+    // eslint-disable-next-line no-undef
+    const CLIENT_ID = process.env.REACT_APP_CLIENT_ID;
+    let navigate = useNavigate();
     const responseGoogle = async (res) => {
         await axios
             .get(
@@ -16,30 +17,30 @@ function App() {
             )
             .then((res) => res.data)
             .then((data) => {
-                window.localStorage.setItem('userdata', JSON.stringify(data))
-                navigate('/formselection')
-            })
+                window.localStorage.setItem('userdata', JSON.stringify(data));
+                navigate('/formselection');
+            });
 
-        console.log(res.profileObj.email)
-    }
+        console.log(res.profileObj.email);
+    };
 
     axios.interceptors.request.use(function (config) {
         // spinning start to show
-        setLoading(true)
-        return config
-    })
+        setLoading(true);
+        return config;
+    });
 
     axios.interceptors.response.use(function (response) {
         // spinning hide
-        setLoading(false)
-        return response
-    })
+        setLoading(false);
+        return response;
+    });
 
     return (
         <div className='App'>
             <Home res={responseGoogle} id={CLIENT_ID} loading={loading} />
         </div>
-    )
+    );
 }
 
-export default App
+export default App;
